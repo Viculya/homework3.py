772c7ad7d3bfa2cb86b9a5887301e81f0940ee84
Viculya <Viculya30@yandex.ru>
2024-07-21 15:39:21 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,8 +1,8 @@
 name="Name: Viktoria"
 print(name)
-age=(37)
-print ("Age:"+(str)(age))
-age=((str)((age)+(1)))
-print(("New age:")+(age))
-is_student= 'Is Student:'
-print('Is Student:',bool(is_student))
+age=37
+print ("Age:"+str(age))
+age=str(age+1)
+print("New age:"+age)
+is_student= True
+print('Is Student:',is_student)
